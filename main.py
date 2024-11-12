7a33fc611c095cf3fa985809f83ceedeeee857dd
UsernameEnterausernameisnotavaible <akmalslipi@gmail.com>
2024-11-13 06:34:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,32 @@ except Exception as e:
 
 # Collect user input
 Age = st.number_input("Usia", min_value=18, max_value=100)
+
 Experience = st.number_input("Jumlah Tahun Pengalaman Kerja", min_value=1, max_value=100)
+
 Family = st.number_input("Jumlah Keluarga", min_value=1, max_value=50)
+
 Education = st.selectbox("Pendidikan Terakhir", options=["1", "2", "3"])
+st.caption(":red[**1**]: Sarjana (Bachelor's degree) :red[**2**]: Magister (Master's degree) :red[**3**]: Profesional (Professional degree)")
+
 Securities_Account = st.selectbox("Apakah memiliki Rekening Investasi?", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 CD_Account = st.selectbox("Apakah memiliki Rekening CD (Sertifikat Deposito)?", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 Online = st.selectbox("Apakah menggunakan layanan Mobile Banking?", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 CreditCard = st.selectbox("Apakah memiliki Kartu Kredit?", options=[0, 1])
+st.caption(":red[**0**] :  Tidak Ada;  :red[**1**] :  Ada")
+
 Income = st.number_input("Jumlah Pendapatan Bulanan (dalam Rupiah)", min_value=0, max_value=500000000)
+
 CCAvg = st.number_input("Jumlah Pengeluaran Bulanan (dalam Rupiah)", min_value=0, max_value=100000000)
+
 Mortgage = st.number_input("Jumlah Hipotek (dalam Rupiah)", min_value=0, max_value=100000000)
+st.caption("Nilai barang yang dijadikan jaminan nasabah kepada bank")
 
 # Create a DataFrame from user input
 input_data = pd.DataFrame({
